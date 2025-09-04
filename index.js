3ccf8b5a3554de79426fad4ec90d09867345331f
Faizalali2002 <127603224+Faizalali2002@users.noreply.github.com>
2025-09-04 17:24:30 +0530

    Initial commit - Node app setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Landing Page");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT} `);
+});
